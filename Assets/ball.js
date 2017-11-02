f402cec7adec959f25718cfac467ae495580f0a8
emon134409 <hasibulkabir@iut-dhaka.edu>
2017-11-02 21:17:49 +0600

    First add

---
+++ Assets/ball.js
@@ -0,0 +1,106 @@
+﻿	#pragma strict
+	import UnityEngine.UI;
+	var thePrefab:GameObject;
+	var throws:boolean;
+	var flag:int;
+	var tb:GameObject;
+	var score:int;
+	var scoretext:Text;
+	var life:int;
+	var lifetext:Text;
+	var gameover:Text;
+	//private var gameController : GameController;
+
+	function Start(){
+	 // var ball:GameObject =Instantiate(thePrefab,transform.position,transform.rotation) as GameObject ;
+	  //flag=0;
+	//  thrower();
+	score=0;
+	life=5;
+	setlifeText(life);
+	setscoreText(score);
+	gameover.text="";
+	}
+
+
+
+	function Update(){
+	if(throws==true){
+		thrower();
+	}
+	}
+
+	function thrower(){
+	var t:player;
+	t=tb.GetComponent("player");
+	var l:Vector3=t.print();
+	//Debug.Log(l);
+	//var lastpos:Vector3=Vector3(491.2,2,305.8);102.4
+	var ballpos:Vector3=Vector3(287.2,90.4,199.2);
+	//var ballpos:Vector3=Vector3(280,90,202.5);
+	var ball:GameObject =Instantiate(thePrefab,ballpos,transform.rotation) as GameObject; 
+	var n:Vector3=Vector3(0,0,30);
+	var g:Vector3=Vector3(0,0,50);
+	// if(flag==1){ 
+	  var e:Vector3=Vector3(480,1.1,360);
+	  if(l.x>e.x){ 
+	  l=l-g;
+	  ball.GetComponent.<Rigidbody>().useGravity=false;
+	  ball.GetComponent.<Rigidbody>().AddForce(l*300);
+	  throws=false;
+	  timer();}
+
+	  else if (l.x<e.x)
+	  {
+	  l=l+n;
+	  ball.GetComponent.<Rigidbody>().useGravity=false;
+	  ball.GetComponent.<Rigidbody>().AddForce(l*300);
+	  throws=false;
+	  timer();
+	  }
+	  }
+
+
+//	  else{
+	// ball.GetComponent.<Rigidbody>().useGravity=false;
+	//  ball.GetComponent.<Rigidbody>().AddForce(lastpos*100);
+	//  throws=false;
+//	  timer();
+	//  flag=1;
+	//  }
+//	}
+
+	function timer(){
+	Debug.Log("okkkkkk");
+		if(throws==false){
+		yield WaitForSeconds(1);
+		throws=true;
+		}
+		score=score+2;
+		setscoreText(score);
+	}
+
+
+	  //ball.AddComponent<Rigidbody>();
+	  //var rb=ball.GetComponet<Rigidbody>();
+	 // rb.AddForce(transform.forward*1000);
+	  //rb= GetComponent<Rigidbody>();
+	 // rb.GetComponent.<Rigidbody>().AddForce(transform.forward*1000);
+	  //rb.AddForce(transform.forward*1000);
+	//var rb = thePrefab.GetComponent.<Rigidbody>();
+
+
+function setlifeText(life){
+if (life==0){
+gameover.text="Game Over";
+yield WaitForSeconds(1);
+Application.LoadLevel (Application.loadedLevel);
+}
+lifetext.text="Life: "+life.ToString();
+
+	}
+
+
+function setscoreText(score){
+	scoretext.text="Score: "+score.ToString();
+	}
---
+++ Assets/player.js
@@ -0,0 +1,64 @@
+﻿#pragma strict
+var position:Vector3;
+var tb:Transform;
+var theball:GameObject;
+
+function Start () {
+position=transform.position;
+//Debug.Log("StarrtPosition"+position);
+}
+function Awake() {
+        Application.targetFrameRate = 200;
+    }
+
+function Update () {
+//Debug.Log("Start");
+var minx:float;
+if(position.z>300 && position.z<370){
+transform.Translate(Input.GetAxis("Horizontal")*150*Time.deltaTime,0f,0f);
+}
+else{
+if(position.z>=370 && Input.GetAxis("Horizontal")<0){
+transform.Translate(Input.GetAxis("Horizontal")*150*Time.deltaTime,0f,0f);
+}
+else if(position.z<=300 && Input.GetAxis("Horizontal")>0){
+transform.Translate(Input.GetAxis("Horizontal")*150*Time.deltaTime,0f,0f);
+}
+
+}
+}
+
+function OnCollisionEnter (col : Collision)
+{
+    var b:ball=theball.GetComponent("ball");
+    if(col.gameObject.name == "ball(Clone)")
+    {
+
+       Destroy(col.gameObject);
+       Debug.Log("Destroyed");
+
+       b.life=(b.life)-1;
+       Debug.Log("Ball:"+b.life );
+       b.setlifeText(b.life);
+
+       b.score=(b.score)-2;
+       Debug.Log("Ball:"+b.score );
+       b.setscoreText(b.score);
+
+
+       // ball.score=(ball.score)-2;
+      //  Debug.Log(ball.score);
+      //  if(ball.score<0){
+      //  ball.score=0;
+      //  }
+     //   ball.SetScoreText(ball.score);
+    }
+    
+}
+
+function print(){
+Debug.Log("Playerrrrrr");
+position=tb.position;
+Debug.Log("FPosition"+position);
+return position;
+}
